b3bf1446e8c19a134281a90dc6d11fdc22bf2069
Rachel Peng <rachelvpeng@gmail.com>
2020-03-21 09:37:12 -0700

    Created React Application

---
+++ mysteam/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="light" size="lg"> api call </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
