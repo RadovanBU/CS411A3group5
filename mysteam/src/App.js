9fa17a35eaf8ff4f1f7b72f92430e7b27d17d59c
Rachel Peng <rachelvpeng@gmail.com>
2020-03-25 11:59:10 -0700

    she's a little messy

--- mysteam/src/App.js
+++ mysteam/src/App.js
@@ -8,7 +8,29 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.url = 'https://www.reddit.com/r/Steam';
-  }
+  };
+
+  state = {
+    result : ""
+  };
+
+  findReddit(q) {
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("http://www.reddit.com/subreddits/search.json?q=" + q + "&sort=new", requestOptions)
+      .then(response => response.text())
+      .then(result => result)
+      .catch(error => console.log('error', error));
+  };
+
+
+  handleChange(event) {
+    console.log("hi");
+    this.findReddit(document.getElementById("game"));
+  };
 
   render() {
     return (
@@ -16,9 +38,10 @@ class App extends React.Component {
         <header className="App-header">
           <h1> Enter Your Game! </h1>
           <form>
-            <input type="text" name="name" />
-            <input type="submit" value="Submit" />
+            <input type="text" name="game" id="game" onChange={this.handleChange}/>
+            {/* <input type="submit" value="Submit"/> */}
           </form>
+          <p> {this.state.result} </p>
         </header>
       </div>
     );
