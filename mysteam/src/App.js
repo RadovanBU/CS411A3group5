84866d43c9c18d919d55854fb54ccb4a4fedeb74
Rachel Peng <rachelvpeng@gmail.com>
2020-03-25 10:32:37 -0700

    add form

--- mysteam/src/App.js
+++ mysteam/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Enter Your Game! </h1>
-        <Button variant="light" size="lg"> Submit </Button>
+        <form>
+          <input type="text" name="name" />
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
