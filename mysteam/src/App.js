3a7004c3458bcfcf594da439dd782508ea344ba9
Rachel Peng <rachelvpeng@gmail.com>
2020-03-25 11:01:21 -0700

    changed to class

--- mysteam/src/App.js
+++ mysteam/src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Enter Your Game! </h1>
-        <form>
-          <input type="text" name="name" />
-          <input type="submit" value="Submit" />
-        </form>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.url = 'https://www.reddit.com/r/Steam';
+  }
+
+  render() {
+    return (
+      <div className = "App">
+        <header className="App-header">
+          <h1> Enter Your Game! </h1>
+          <form>
+            <input type="text" name="name" />
+            <input type="submit" value="Submit" />
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
