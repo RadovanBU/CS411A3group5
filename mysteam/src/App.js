df490f3a6e60104b9d894a1150dfe8b3303cc471
Rachel Peng <rachelvpeng@gmail.com>
2020-03-25 13:33:12 -0700

    yayeet

--- mysteam/src/App.js
+++ mysteam/src/App.js
@@ -10,55 +10,81 @@ class App extends React.Component {
 
     this.state = {
       subreddits: [],
-      parsedData: []
+      parsedData: [],
+      submitted: false
     };
   };
 
   async findReddit(query) {
-  let requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+    this.setState({ subreddits: [] })
+
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    await fetch("http://www.reddit.com/subreddits/search.json?q=" + query + "&sort=new", requestOptions)
+      .then(res => res.json())
+      .then(json => this.setState({ subreddits: json }))
+      .catch(error => console.log('error', error));
+
+    let lenvar = this.state.subreddits.data.children.length;
+
+    for (let i = 0; i < lenvar; i++) {
+      this.state.parsedData.push(this.state.subreddits.data.children[i].data.display_name_prefixed);
+    }
+
+    console.log(this.state.parsedData);
+
+    this.setState({ submitted: true })
   };
 
-  await fetch("http://www.reddit.com/subreddits/search.json?q=" + query + "&sort=new", requestOptions)
-    .then(res => res.json())
-    .then(json => this.setState({ subreddits: json }))
-    .catch(error => console.log('error', error));
+  handleSubmit = event => {
+    event.preventDefault();
+    this.findReddit(this.input.value)
+  };
 
-  //console.log(this.state.subreddits.data.children[0].data.display_name_prefixed);
-  let lenvar = this.state.subreddits.data.children.length;
+  render() {
+    if (this.state.submitted) {
+      const listItems = this.state.parsedData.map((link) =>
+        <li>{link}</li>
+      );
+      
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1> Enter Your Game! </h1>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                name="game"
+                defaultValue="Steam"
+                ref={(input) => this.input = input}
+              />
+            </form>
+            <ul>{listItems}</ul>
+          </header>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1> Enter Your Game! </h1>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                name="game"
+                defaultValue="Steam"
+                ref={(input) => this.input = input}
+              />
+            </form>
+          </header>
+        </div>
+      );
 
-  for (let i = 0; i<lenvar; i++){
-    this.state.parsedData.push(this.state.subreddits.data.children[i].data.display_name_prefixed);
+    }
   }
-
-  console.log(this.state.parsedData);
-};
-
-handleSubmit = event => {
-  event.preventDefault();
-  this.findReddit(this.input.value)
-};
-
-render() {
-  console.log(this.state.subreddits)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Enter Your Game! </h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="game"
-            defaultValue="Steam"
-            ref={(input) => this.input = input}
-          />
-        </form>
-
-      </header>
-    </div>
-  );
-}
 }
 
 export default App;
