8ca7774ccc993ca13bb13653b9204016fa63f8f5
schen000 <schen000@bu.edu>
2020-04-18 14:32:11 -0500

    Steam Auth Complete

--- MERN App/config/passport.js
+++ MERN App/config/passport.js
@@ -11,11 +11,11 @@ module.exports = (passport) => {
         profile: true // set to false to skip fetching data from the Steam Web API, removing need for API key
       },
       function(identifier, profile, done) {
-          console.log(identifier)
           console.log(profile)
-        // User.findByOpenID({ openId: identifier }, function (err, user) {
-        //   return done(err, user);
-        // });
+          process.nextTick(function () {
+            return done(null, profile);
+          });
+        
       }
     ));
 }
