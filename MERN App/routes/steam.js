09ebc60e9b4274c881de44363f813e57efecfc41
RadovanBU <rvitek@bu.edu>
2020-04-24 17:41:16 -0400

    nearly done

--- MERN App/routes/steam.js
+++ MERN App/routes/steam.js
@@ -17,9 +17,12 @@ router.get('/', function (req, res, next) {
 
   let mongo = db.getDB();
   let steamGames = new Array();
+  let redditAPIresults = new Array();
 
   mongo.collection("users").find({ _id: 1 }).toArray(function (err, result) {
     if (err) throw err;
+
+
 
     fetch("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=" + key.apiKey + "&steamid=" + result[0].steamId + "&format=json")
 
@@ -36,11 +39,52 @@ router.get('/', function (req, res, next) {
 
           console.log(steamGames);
 
-          // res.render('dbRes', { dbRes: `You have played ${steamGames.toString()} in the past two weeks.` });
+          //Reddit API Call on Steam games
 
+          let steamLen = steamGames.length;
+          for (let i = 0; i<steamLen;i++) {
+            let currentGame = steamGames[i];
+
+            let db_return = mongo.collection("games").find({"redditName": currentGame}).each(function (err, result) {
+
+              let redditJSONsteam = result;
+
+              //If something found in DB we use those results
+              if (redditJSONsteam) {
+                redditAPIresults.push(result.reddits);
+              }
+
+              //If not move onto API
+              else {
+                fetch("http://www.reddit.com/subreddits/search.json?q=" + currentGame + "&sort=new")
+                    .then(response => {
+                      return response.json();
+                    })
+
+                    .then(json => {
+                      lenvar = json.data.children.length;
+                      for (let i = 0; i < lenvar; i++) {
+                        console.log(json.data.children[i].data.display_name_prefixed);
+                        redditAPIresults.push(json.data.children[i].data.display_name_prefixed);
+                      }
+                    })
+
+                    .catch(err => {
+                      res.render('index', {title: "An error has occured"});
+                    })
+              }
+            });
+
+
+            // res.render('dbRes', { dbRes: `You have played ${steamGames.toString()} in the past two weeks.` });
+
+          }
+
+          res.render('dbRes', {dbRes: `We suggest these reddits ${redditAPIresults.toString()} `});
+          //res.write('steamAPI', {steamRes: `We suggest these reddits ${redditAPIresults.toString()} ` });
         } else {
           console.log("Not enough games played on Steam")
-          // res.render('dbRes', { dbRes: `You have played have not played any Steam games in the past two weeks.` });
+          //res.render('dbRes', { dbRes: `You have played have not played any Steam games in the past two weeks.` });
         }
       })
     .catch(err => {
